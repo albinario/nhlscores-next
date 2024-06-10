87e0d20c9d19315bd92c35f1dac782328937b9ce
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-06-10 22:41:09 +0200

    no cache

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -2,7 +2,9 @@ import useSWR from 'swr'
 
 export default function useFetchData<T>(queryKey: string) {
 	const fetcher = async (endpoint: string) => {
-		const res = await fetch('/api/' + endpoint)
+		const res = await fetch('/api/' + endpoint, {
+			headers: { 'Cache-Control': 'no-store, max-age=0' },
+		})
 		const data = await res.json()
 		return data as T
 	}

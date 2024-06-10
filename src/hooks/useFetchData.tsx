54b3113513e8b260cf28ea64152f223cca44f371
Albin Lindeborg <albin.lindeborg@gmail.com>
2024-06-10 23:04:36 +0200

    remove cache tests

--- src/hooks/useFetchData.tsx
+++ src/hooks/useFetchData.tsx
@@ -2,9 +2,7 @@ import useSWR from 'swr'
 
 export default function useFetchData<T>(queryKey: string) {
 	const fetcher = async (endpoint: string) => {
-		const res = await fetch('/api/' + endpoint, {
-			headers: { 'Cache-Control': 'no-store, max-age=0' },
-		})
+		const res = await fetch('/api/' + endpoint)
 		const data = await res.json()
 		return data as T
 	}
